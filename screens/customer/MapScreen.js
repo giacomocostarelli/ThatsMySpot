ac8fca6e81927f55cae6ec85a822e51f7bf6169f
@giacomo.costarelli <giacomo.costarelli39@gmail.com>
2021-12-09 20:38:49 +0100

    Introduce <MapView> + Location. Add Traffic and back to current location Button.

--- screens/customer/MapScreen.js
+++ screens/customer/MapScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, setState } from "react";
 import {
 	View,
 	Text,
@@ -6,21 +6,68 @@ import {
 	Button,
 	StyleSheet,
 	ActivityIndicator,
+	Dimensions,
 } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import * as Location from "expo-location";
+import MapView from "react-native-maps";
 
 const MapScreen = (props) => {
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [initial, setInitial] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			let { status } = await Location.requestForegroundPermissionsAsync();
+			if (status !== "granted") {
+				setErrorMsg("Permission to access location was denied");
+				return;
+			}
+			let location = await Location.getCurrentPositionAsync({});
+			setLocation(location);
+			let text = "Waiting..";
+			if (errorMsg) {
+				text = errorMsg;
+			} else if (location) {
+				text = JSON.stringify(location);
+				setInitial({
+					latitude: location.coords.latitude,
+					longitude: location.coords.longitude,
+					latitudeDelta: 0.0025,
+					longitudeDelta: 0.0025,
+				});
+			}
+		})();
+	}, []);
+
 	return (
-		<View style={styles.centered}>
-			<Text>MapScreen</Text>
+		<View style={styles.container}>
+			{initial && (
+				<MapView
+					loadingEnabled={true}
+					showsTraffic={true}
+					mapPadding={{ top: 100 }}
+					showsMyLocationButton={true}
+					showsUserLocation={true}
+					initialRegion={initial}
+					style={styles.map}
+				/>
+			)}
 		</View>
 	);
 };
 
 const styles = StyleSheet.create({
-	centered: {
+	container: {
 		flex: 1,
+		backgroundColor: "#fff",
 		alignItems: "center",
 		justifyContent: "center",
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height,
 	},
 });
 
